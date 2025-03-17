84b507bd324fa5d0d673b8a6cb4bad76bb1df95a
Pantelis Ts. <102387490+PantelisTsagkas@users.noreply.github.com>
2025-03-17 10:56:29 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Hello Pantelis! Well done!"));
+app.get("/", (req, res) => res.send("Hello  Java!"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
