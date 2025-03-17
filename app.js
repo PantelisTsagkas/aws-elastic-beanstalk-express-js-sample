b5609b989d7f11f38041e93afa71a9781aa8f1cd
pantelis <pantelis.tsagkas@jet2.com>
2025-03-17 11:01:24 +0000

    another push to remote

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Hello  Java!"));
+app.get("/", (req, res) => res.send("My name is Pantelis and I am learning AWS!"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
